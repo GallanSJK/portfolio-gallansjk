caaab4f97261caad1c659f70290fe5ee89bb921c
GallanSJK <gallansteffan99@gmail.com>
2021-10-14 12:44:56 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,322 @@
+<!DOCTYPE html>
+<html lang="en" id="home">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Portfolio Gallan Stefan J.K.</title>
+
+    <!-- Bootstrap -->
+    <link rel="icon" href="img/logo2.jpg" sizes="32x32">
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="bootstrap/css/style.css">
+
+  </head>
+  <body>
+
+    <!-- Awal Navbar -->
+    <div class="navbar navbar-inverse navbar-fixed-top" style="background-color: #1100ff;">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a href="index.html" class="navbar-brand page-scroll">GALLAN S.J.K.</a>
+        </div>
+
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#home" class="page-scroll">Home</a></li>
+            <li><a href="#about" class="page-scroll">About</a></li>
+            <li><a href="#portfolio" class="page-scroll">Portfolio</a></li>
+            <li><a href="#blog" class="page-scroll">Blog</a></li>
+            <li><a href="#team" class="page-scroll">Team</a></li>
+            <li><a href="#contact" class="page-scroll">Contact</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <!-- Akhir Navbar -->
+    
+    <!-- jumbotron -->
+    <div class="jumbotron text-center">
+      <img src="img/logo2.jpg" class="img-circle">
+      <h1>Gallan Stefan Jaya Kelana</h1>
+      <p>Programmer | Gammer | Volleyball Lover</p>
+      <a href="https://www.youtube.com/channel/UC-q-H4wulQqtb6A1ng9i87Q" class="btn btn-danger"  target="_new"><i class="glyphicon glyphicon-play"></i>Subscribe: Voli Dunia</a>
+    </div>
+    <!-- akhir jumbotron -->
+
+    <!-- about -->
+    <section class="about" id="about">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12">
+            <h2 class="text-center">About</h2>
+            <hr>
+          </div> 
+        </div>
+
+        <div class="row">
+          <div class="col-sm-4">
+            <p><img src="img/portfolio/gallan.png" class="img-circle"></p>
+          </div>
+          <div class="col-sm-4">
+            <p>
+                Nama : Gallan Stefan Jaya Kelana<br><br>
+                Tanggal Lahir : 06 September 1999<br><br>
+                Usia : 22 Tahun<br><br>
+                Alamat : Bukit Gading Cisoka Blok E 16 RT03/RW05 Desa Selapajang Kecamatan Cisoka Kabupaten Tangerang Provinsi Banten<br><br>
+                Hobi : Voli, Game Online dan Mendaki Gunung<br><br>
+                Profesi : Freelancer Programmer
+            </p>
+          </div>
+          <div class="col-sm-4">
+            <p>Sebagai Pengembang Web, saya selalu bersemangat untuk belajar tentang terobosan baru dalam teknologi. Saya juga memiliki keterampilan interpersonal yang sangat baik yang telah membantu saya untuk memimpin berbagai proyek dan juga membuat saya dapat menangani proyek dalam pertemuan.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- akhir about -->
+
+    <!-- portfolio -->
+    <section class="portfolio" id="portfolio">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12">
+            <h2 class="text-center">Portfolio</h2>
+            <hr>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-sm-3">
+            <a href="" class="thumbnail">
+              <img src="img/portfolio/1.png">
+            </a>
+          </div>
+
+          <div class="col-sm-3">
+            <a href="" class="thumbnail">
+              <img src="img/portfolio/2.png">
+            </a>
+          </div>
+
+          <div class="col-sm-3">
+            <a href="" class="thumbnail">
+              <img src="img/portfolio/3.png">
+            </a>
+          </div>
+
+          <div class="col-sm-3">
+            <a href="" class="thumbnail">
+              <img src="img/portfolio/4.png">
+            </a>
+          </div>
+
+          <div class="col-sm-3">
+            <a href="" class="thumbnail">
+              <img src="img/portfolio/5.png">
+            </a>
+          </div>
+
+          <div class="col-sm-3">
+            <a href="" class="thumbnail">
+              <img src="img/portfolio/6.png">
+            </a>
+          </div>
+
+          <div class="col-sm-3">
+            <a href="" class="thumbnail">
+              <img src="img/portfolio/7.png">
+            </a>
+          </div>
+
+          <div class="col-sm-3">
+            <a href="" class="thumbnail">
+              <img src="img/portfolio/8.png">
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- akhir portfolio -->
+
+    <!-- Blog -->
+    <section class="blog" id="blog">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12 text-center">
+            <h2>Blog</h2>
+            <hr>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-sm-4">
+            <a href="" class="thumbnail">
+              <img src="img/portfolio/7.png">
+            </a>
+            <p class="card-text text-center">Kebersamaan antar pendaki pada puncak gunung prau via patak banteng yang beranggotakan 8 orang dari kota yang berbeda.</p>
+            <a href="#" class="btn btn-primary center-block">Go somewhere</a>
+            <p></p>
+          </div>
+
+          <div class="col-sm-4">
+            <a href="" class="thumbnail">
+              <img src="img/portfolio/4.png">
+            </a>
+            <p class="card-text text-center">Perjalanan mendaki ke gunung slamet via bambangan bersama 3 rekan pecinta alam dari universitas yang berbeda.</p>
+            <a href="#" class="btn btn-primary center-block">Go somewhere</a>
+            <p></p>
+          </div>
+
+          <div class="col-sm-4">
+            <a href="" class="thumbnail">
+              <img src="img/portfolio/8.png">
+            </a>
+            <p class="card-text text-center">Semua perjalanan selalu di nikmati dalam pendakian ke gunung prau via patak banteng, dan semua lelah terbayarkan ketika mencapai puncak.</p>
+            <a href="#" class="btn btn-primary center-block">Go somewhere</a>
+            <p></p>
+          </div>
+          
+
+        </div>
+      </div>
+    </section>  
+    <!-- akhir blog -->
+
+    <!-- Team -->
+    <section class="team" id="team">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12 text-center">
+            <h2>Team</h2>
+            <hr>
+          </div>
+        </div>
+
+        <div class="row">
+          <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+              <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+              <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+              <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+            </ol>
+
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner" role="listbox">
+              <div class="item active">
+                <center>
+                <img src="img/gallan1.png" alt="...">
+                </center>
+                <div class="carousel-caption">
+                  <h3>GALLAN</h3>
+                  <p>PROGRAMMER</p>
+                </div>
+              </div>
+              <div class="item">
+                <center>
+                <img src="img/gallan1.png" alt="...">
+                </center>
+                <div class="carousel-caption">
+                  <h3>STEFAN</h3>
+                  <p>UI/UX DESIGNER</p>
+                </div>
+              </div>
+              <div class="item">
+                <center>
+                <img src="img/gallan1.png" alt="...">
+                </center>
+                <div class="carousel-caption">
+                  <h3>JAYA K.</h3>
+                  <p>QUALITY ASSURANCE</p>
+                </div>
+              </div>
+              
+            </div>
+
+            <!-- Controls -->
+            <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+              <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+              <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
+          </div>
+
+        </div>
+
+      </div>
+    </section>
+    <!-- akhir team -->
+    
+    <!-- contact -->
+    <section class="contact" id="contact">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12 text-center">
+            <h2>Contact Us</h2>
+            <hr>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-sm-6 col-sm-offset-3">
+            <form>
+              <div class="form-group">
+                <label for="nama">Nama</label>
+                <input type="text" name="" id="nama" class="form-control" placeholder="nama anda">
+              </div>
+
+              <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" name="" id="email" class="form-control" placeholder="email aktif anda">
+              </div>
+
+              <div class="form-group">
+                <label for="pesan">Pesan</label>
+                <textarea class="form-control" id="pesan" rows="10" placeholder="ketikkan pesan anda"></textarea>
+              </div>
+
+              <button type="submit" class="btn btn-info"><i class="glyphicon glyphicon-send"></i> Kirim Pesan</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- akhir contact -->
+
+    <!-- footer -->
+    <footer>
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-sm-12">
+            <p>&copy; Copyright 2021 | <a href="https://www.instagram.com/gallansjk"  target="_new">Instagram : gallansjk</a></p>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-sm-12">
+            <a href="https://wa.me/6282260646099?text=Saya%20tertarik%20dengan%20website%20yang%20Anda%20buat" class="btn btn-success"  target="_new"><i class="glyphicon glyphicon-phone"></i> 0822-6064-6099</a>
+          </div>  
+        </div>
+      </div>
+    </footer>
+    <!-- akhir footer -->
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="bootstrap/js/jquery-3.4.1.min.js"></script>
+    <script src="bootstrap/js/jquery.easing.1.3.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+
+    <script src="bootstrap/js/script.js"></script>
+  </body>
+</html>
