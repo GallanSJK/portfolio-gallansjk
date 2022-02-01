603237b1454aac6ba8c94870da7cf791c5fd27fc
Gallan Stefan Jaya Kelana <90021411+GallanSJK@users.noreply.github.com>
2022-02-01 20:49:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Portfolio Gallan Stefan J.K.</title>
+    <title>Portfolio Gallan</title>
 
     <!-- Bootstrap -->
     <link rel="icon" href="img/logo2.jpg" sizes="32x32">
